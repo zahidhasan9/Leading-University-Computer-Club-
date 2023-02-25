b8d844494d815ebd0f35c9026b860b741cf2ad63
zahidhasan9 <44033232+zahidhasan9@users.noreply.github.com>
2023-02-26 01:58:55 +0600

    fast

---
+++ LUCC font end 2 by finsa 10-4-22/src/components/sections/portfoliodetail/Content.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from 'react';
+import Cta from './Cta';
+import Details from './Details';
+
+class Content extends Component {
+    render() {
+        return (
+            <Fragment>
+                <Details/>
+                {/* <Cta/> */}
+            </Fragment>
+        );
+    }
+}
+
+export default Content;
